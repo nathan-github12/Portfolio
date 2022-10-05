f51f7e38783f496d97e2dbde5ad17d9e9674f48b
Nathan Coburn <57078703+nathan-github12@users.noreply.github.com>
2022-10-05 17:51:56 +0100

    Update connect4.c

--- Connect 4/connect4.c
+++ Connect 4/connect4.c
@@ -1,6 +1,3 @@
-//You can include any of headers defined in the C11 standard here. They are:
-//assert.h, complex.h, ctype.h, errno.h, fenv.h, float.h, inttypes.h, iso646.h, limits.h, locale.h, math.h, setjmp.h, signal.h, stdalign.h, stdarg.h, stdatomic.h, stdbool.h, stddef.h, stdint.h, stdio.h, stdlib.h, stdnoreturn.h, string.h, tgmath.h, threads.h, time.h, uchar.h, wchar.h or wctype.h
-//You may not include any other headers.
 
 #include<stdio.h>
 #include<stdlib.h>
